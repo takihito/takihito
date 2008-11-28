bc29236d01bd568b22d8a9ce0756d17086bbf61d
akihito <takeda.akihito@gmail.com>
2008-11-28 10:59:48 +0900

    added File-Stat-Trigger

---
+++ perl/File-Stat-Trigger/lib/File/Stat/Trigger.pm
@@ -0,0 +1,33 @@
+package File::Stat::Trigger;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+File::Stat::Trigger -
+
+=head1 SYNOPSIS
+
+  use File::Stat::Trigger;
+
+=head1 DESCRIPTION
+
+File::Stat::Trigger is
+
+=head1 AUTHOR
+
+Akihito Takeda E<lt>takeda.akihito@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
