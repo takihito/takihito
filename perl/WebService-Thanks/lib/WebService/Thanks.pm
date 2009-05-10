845f0f030fcedeaad67c2190e4f75c59d3a4d38d
竹田 昭仁 <takeda.akihito@gmail.com>
2009-05-10 21:10:54 +0900

    start WebService::Thanks

---
+++ perl/WebService-Thanks/lib/WebService/Thanks.pm
@@ -0,0 +1,33 @@
+package WebService::Thanks;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+WebService::Thanks -
+
+=head1 SYNOPSIS
+
+  use WebService::Thanks;
+
+=head1 DESCRIPTION
+
+WebService::Thanks is
+
+=head1 AUTHOR
+
+Akihito Takeda E<lt>takeda.akihito@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
